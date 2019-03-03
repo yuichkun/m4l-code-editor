43c772c24088f7915f3dac0a778b0a66a9e1cc1c
Yuichi Yogo <yuichkun@gmail.com>
2019-03-04 02:34:02 +0900

    chore

--- client/src/constants/initialEditorArea.js
+++ client/src/constants/initialEditorArea.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default `
 (midievent) => {
   const dryNote = withDelay(0)(midievent);
@@ -5,7 +6,7 @@ export default `
 
   for(let i = 0; i < 20; i++){
     const rndInterval = Math.floor(Math.random() * 10) - 5;
-    const newNote = withDelay(0)(transpose(rndInterval)(midievent));
+    const newNote = withDelay(0)(transpose(rndInterval)(midievent));
     notes.push(newNote);
   }
 
